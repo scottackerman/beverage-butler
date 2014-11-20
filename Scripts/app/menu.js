ee98b27b51e8137d163555cb194da8455939feac
ackerman <scott.ackerman@icfi.com>
2014-11-20 11:55:15 -0500

    removing old functions

--- Scripts/app/menu.js
+++ Scripts/app/menu.js
@@ -43,7 +43,6 @@ App.Global = (function (window, document) {
 		},
 
 		'initConnections': function () {
-			//alert('about to init connections');
 			connection = $.connection('http://192.168.1.100:8081/browser');
 			connectinArduino = $.connection('http://192.168.1.100:8081/arduino');
 
